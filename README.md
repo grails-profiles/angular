6f57d2f1d9c5d1798cdcbd745a5197570c0e04b0
Will Buck <william.parker.buck@gmail.com>
2016-09-01 00:09:13 -0500

    First pass of working webpack example from ng2 site

---
+++ README.md
@@ -0,0 +1,6 @@
+TODO
+
+- Figure out integrating karma testing into grails pipeline
+- Figure out how to manage index.html and index.gsp parity
+- Figure out webpack dev server integration?
+- Figure out copying over css / images files from src to assets (or something similar)
---
+++ skeleton/src/main/ts/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import '../../../../grails-app/assets/stylesheets/ng2.css';
+
+@Component({
+    selector: 'sample-ng2-app',
+    templateUrl: './app.component.html',
+    styleUrls: [String('./app.component.css')]
+})
+export class AppComponent { }
