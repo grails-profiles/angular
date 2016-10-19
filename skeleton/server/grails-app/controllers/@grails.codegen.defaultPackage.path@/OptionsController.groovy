a372e37c2141b73ee760d922d0e97200743d0360
jameskleeh <james.kleeh@gmail.com>
2016-10-19 16:10:17 -0400

    Add default options mapping and controller

---
+++ skeleton/server/grails-app/controllers/@grails.codegen.defaultPackage.path@/OptionsController.groovy
@@ -0,0 +1,8 @@
+package @grails.codegen.defaultPackage@
+
+class OptionsController {
+
+    def index() {
+        render(status: 204)
+    }
+}
---
+++ skeleton/server/grails-app/controllers/@grails.codegen.defaultPackage.path@/UrlMappings.groovy
@@ -0,0 +1,18 @@
+package @grails.codegen.defaultPackage@
+
+class UrlMappings {
+
+    static mappings = {
+        delete "/$controller/$id(.$format)?"(action:"delete")
+        get "/$controller(.$format)?"(action:"index")
+        get "/$controller/$id(.$format)?"(action:"show")
+        post "/$controller(.$format)?"(action:"save")
+        put "/$controller/$id(.$format)?"(action:"update")
+        patch "/$controller/$id(.$format)?"(action:"patch")
+        options "/**"(controller: 'options')
+
+        "/"(controller: 'application', action:'index')
+        "500"(view: '/error')
+        "404"(view: '/notFound')
+    }
+}
